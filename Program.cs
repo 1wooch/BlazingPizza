1af2f7a7b4a4fa605cd6835489910de37a587e0d
wwc <onechoo1106@gmail.com>
2023-06-18 00:18:47 +1000

    init commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using BlazingPizza.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
@@ -16,5 +19,15 @@ app.UseRouting();
 app.MapRazorPages();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+
+var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+using (var scope = scopeFactory.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
+    if (db.Database.EnsureCreated())
+    {
+        SeedData.Initialize(db);
+    }
+}
 
 app.Run();
